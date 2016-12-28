cdbf6fe524f5b12afd58c1fb696d8ef895e73da9
102820021 <102820021wsx987@gmail.com>
2016-12-28 11:53:09 +0800

    Small change

--- IssueTrackingSystem/Model/DataModel/Server.cs
+++ IssueTrackingSystem/Model/DataModel/Server.cs
@@ -8,7 +8,7 @@ namespace IssueTrackingSystem.Model.DataModel
 {
     class Server
     {
-        private static String apiUrl = "http://cic01.mmslab.ntut.edu.tw:8080/ITS_ws/api";
+        private static String apiUrl = "https://140.124.73.12:8443/ITS_ws/api";
 
         public static String ApiUrl
         {
--- IssueTrackingSystem/Model/ProjectMemberModel.cs
+++ IssueTrackingSystem/Model/ProjectMemberModel.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using IssueTrackingSystem.Model.ApiModel;
+using IssueTrackingSystem.Model.DataModel;
+using System.Net;
+using System.IO;
+using Chsword;
 
 namespace IssueTrackingSystem.Model
 {
     class ProjectMemberModel
     {
+        public int createMember(ProjectMember member)
+        {
+            int state = 0;
+            var req = WebRequest.Create(Server.ApiUrl + "/members/" + member.UserId + "/" + member.ProjectId);
+            req.Method = "POST";
+            req.ContentType = "application/json";
+            String contentData = "{\"userId\":\"" + member.UserId + "\"," +
+                                  "\"role\":\"" + member.Role + "\"}";
+            using (var writer = new StreamWriter(req.GetRequestStream()))
+            {
+                writer.Write(contentData);
+            }
+
+            var resp = (HttpWebResponse)req.GetResponse();
+            using (var reader = new StreamReader(resp.GetResponseStream()))
+            {
+                var membertData = reader.ReadToEnd();
+                dynamic memberApiModel = JsonConvert.DeserializeObject<dynamic>(membertData);
+                state = memberApiModel.State;
+            }
+
+            return state;
+        }
+
+        public List<ProjectMember> getMemberByProjectId(ProjectMember member)
+        {
+            List<ProjectMember> memberList = null;
+/*            var req = WebRequest.Create(Server.ApiUrl + "/members/list/" + member.UserId + "/" + member.ProjectId);
+            req.Method = "GET";
+            var resp = (HttpWebResponse)req.GetResponse();
+            using (var reader = new StreamReader(resp.GetResponseStream()))
+            {
+                var membertData = reader.ReadToEnd();
+                dynamic memberApiModel = JsonConvert.DeserializeObject<dynamic>(membertData);
+                state = memberApiModel.State;
+            }*/
+            return memberList;
+        }
     }
 }
