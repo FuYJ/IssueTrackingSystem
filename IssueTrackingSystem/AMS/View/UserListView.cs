92b1e128bacb340f8d3bd87ee497bb5ff067800d
FuYJ <zz28234830@gmail.com>
2016-12-31 23:12:12 +0800

    add update&delete user by systemManager

--- IssueTrackingSystem/AMS/View/UserListView.cs
+++ IssueTrackingSystem/AMS/View/UserListView.cs
@@ -29,30 +29,28 @@ namespace IssueTrackingSystem.AMS.View
             this.issueModel = issueModel;
             this.projectModel = projectModel;
             this.projectMemberModel = projectMemberModel;
+            userModel.userDataChanged += updateUserList;
+            issueModel.issueDataChanged += updateUserList;
+            projectModel.projectDataChanged += updateUserList;
             securityController = new SecurityController(userModel);
             userController = new UserController(userModel);
-
-            userList = securityController.listAccounts();
         }
 
         private void AccountListViewLoad(object sender, EventArgs e)
         {
-            foreach (User user in userList)
-            {
-                String userRole = null;
-                if (user.Authority == (int)User.AuthorityEnum.GeneralUser)
-                    userRole = "一般使用者";
-                else
-                    userRole = "系統管理員";
-                userListDataGridView.Rows.Add(new Object[] { user.UserId, user.UserName, user.EmailAddress, userRole, user.JoinedProjects.Count, user.InvitedProjects.Count, user.Issues.Count, "刪除" });
-            }
+            updateUserList();
         }
 
         private void userRoleValueChanged(object sender, DataGridViewCellEventArgs e)
         {
-            if (e.ColumnIndex == 3 && e.RowIndex >= 0) {
-                User user = userModel.getUserInfo(int.Parse((String)userListDataGridView.Rows[e.RowIndex].Cells[0].Value));
-                
+            if (e.ColumnIndex == 3 && e.RowIndex >= 0 && e.RowIndex < userListDataGridView.RowCount)
+            {
+                User user = userModel.getUserInfo((int)userListDataGridView.Rows[e.RowIndex].Cells[0].Value);
+                if ((String)userListDataGridView.Rows[e.RowIndex].Cells[3].Value == "系統管理員")
+                    user.Authority = (int)User.AuthorityEnum.SystemManager;
+                else
+                    user.Authority = (int)User.AuthorityEnum.GeneralUser;
+                userController.updateUser(user);
             }
         }
 
@@ -62,6 +60,20 @@ namespace IssueTrackingSystem.AMS.View
             {
                 int state = 0;
                 state = userController.deleteUser((int)userListDataGridView.Rows[e.RowIndex].Cells[0].Value);
+            }
+        }
+
+        private void updateUserList(){
+            userList = securityController.listAccounts();
+            userListDataGridView.Rows.Clear();
+            foreach (User user in userList)
+            {
+                String userRole = null;
+                if (user.Authority == (int)User.AuthorityEnum.GeneralUser)
+                    userRole = "一般使用者";
+                else
+                    userRole = "系統管理員";
+                userListDataGridView.Rows.Add(new Object[] { user.UserId, user.UserName, user.EmailAddress, userRole, user.JoinedProjects.Count, user.InvitedProjects.Count, user.Issues.Count, "刪除" });
             }
         }
     }
